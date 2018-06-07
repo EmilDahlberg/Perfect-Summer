35965b96798cb1029d9350ce5b54c7932ccc45e3
FrozenWaffle <theonlydallen@gmail.com>
2018-06-07 13:44:58 +0200

    Added functionality to switch between mouse and controller

--- Assets/GameManagerController.cs
+++ Assets/GameManagerController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManagerController : MonoBehaviour {
+    [Header("Controllers")]
     public bool usingController;
     public bool usingKeyboard;
 
+    [Header("Options")]
+    public bool lockCursor;
+
 	void Start () {
-		
+        if (lockCursor) {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }	
 	}
 	
 	// Update is called once per frame
--- Assets/PlayerCameraController.cs
+++ Assets/PlayerCameraController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCameraController : MonoBehaviour {
+    public float sensitivity = 10f;
+    public float maxYAngle = 80f;
+    private Vector2 currentRotation;
 
     private GameManagerController gameManager;
     private float rotateSpeed = 0.5f;
@@ -24,17 +27,14 @@ public class PlayerCameraController : MonoBehaviour {
     }
 
     void MouseInputCamera() {
-        //     public float sensitivity = 10f;
-        //     public float maxYAngle = 80f;
-        //private Vector2 currentRotation;
-   
-        //currentRotation.x += Input.GetAxis("Mouse X") * sensitivity;
-        //currentRotation.y -= Input.GetAxis("Mouse Y") * sensitivity;
-        //currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
-        //currentRotation.y = Mathf.Clamp(currentRotation.y, -maxYAngle, maxYAngle);
-        //Camera.main.transform.rotation = Quaternion.Euler(currentRotation.y, currentRotation.x, 0);
-        //if (Input.GetMouseButtonDown(0))
-        //    Cursor.lockState = CursorLockMode.Locked;
+    
+        currentRotation.x += Input.GetAxis("Mouse X") * sensitivity;
+        currentRotation.y -= Input.GetAxis("Mouse Y") * sensitivity;
+        currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
+        currentRotation.y = Mathf.Clamp(currentRotation.y, -maxYAngle, maxYAngle);
+        transform.rotation = Quaternion.Euler(currentRotation.y, currentRotation.x, 0);
+        if (Input.GetMouseButtonDown(0))
+            Cursor.lockState = CursorLockMode.Locked;
     }
 
     void JoyStickCamera() {
